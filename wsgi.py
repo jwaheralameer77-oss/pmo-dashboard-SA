9dfa5234bd8d47a5fcc9f567cb4c578a64e79703
PMO Admin <Eamynass@gmail.com>
2026-04-13 18:29:44 +0300

    Fix wsgi.py for Render deployment

--- wsgi.py
+++ wsgi.py
@@ -1,16 +1,24 @@
 import sys
 import os
 
+# Get the current directory
+current_dir = os.path.dirname(os.path.abspath(__file__))
+
 # Add project directory to path
-path = '/home/yourusername/pmo_app'
-if path not in sys.path:
-    sys.path.insert(0, path)
+if current_dir not in sys.path:
+    sys.path.insert(0, current_dir)
 
 # Change to project directory
-os.chdir(path)
+os.chdir(current_dir)
 
 # Import app
 from app import app as application, init_db
 
 # Initialize database on first load
-init_db()
+try:
+    init_db()
+except:
+    pass
+
+# For Gunicorn
+app = application
